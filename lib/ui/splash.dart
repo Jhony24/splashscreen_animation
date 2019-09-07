6e3e3497a6b3d980b2555dda3f661fc5b564eaaf
iang12 <ianwandersong12@gmail.com>
2019-09-07 11:09:40 -0300

    fisrt commit

---
+++ lib/ui/splash.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_mega_filmes/ui/home.dart';
+import 'package:flutter_mega_filmes/ui/loader_animator.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    
+    super.initState();
+    Timer(Duration(seconds: 6),()=>Navigator.push(context, MaterialPageRoute(builder: (BuildContext context){
+      return Home();
+    })));
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color.fromRGBO(20, 26, 50, 1),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+        Image.asset('images/splash.png'),
+        SizedBox(height: 40.0,),
+        Image.asset('images/logo_app.jpeg',width: 150.0,),
+        Loading(
+          radius: 15.0,
+          dotRadius: 6.0,
+        )
+      ],),
+    );
+  }
+}
